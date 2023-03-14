bb821635b96370c4b57ebdb83948d485456455aa
Rudra Bisht <rudra.bisht101@gmail.com>
2023-03-14 14:05:14 +0530

    Used bcrypt for salting and hashing the psw

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const port = 3000 || process.env.PORT;
 
@@ -39,27 +40,33 @@ app.get("/register", function(req, res){
 })
 
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    newUser.save().then(() => {
-        res.render("secrets");
-    }).catch((err) => {
-        console.log(err);
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save().then(() => {
+            res.render("secrets");
+        }).catch((err) => {
+            console.log(err);
+        });
     });
 })
 
 app.post("/login", function(req, res){
     const username = req.body.username;
     const password = req.body.password;
+
     User.findOne({email: username}).then((found) => {
         if(found){
-            if(found.password === md5(password)) {
-                res.render("secrets");
-            } else {
-                res.send("Incorrect password.")
-            }
+            bcrypt.compare(password, found.password, function(err, result) {
+                if(result === true) {
+                    res.render("secrets");
+                } else {
+                    res.send("Incorrect Password!")
+                }
+            });
         } else {
             res.send("Oops! No such user found.")
         }
