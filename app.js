a347d0acac0cd42cf5d94244b55a7edf4e442e41
Rudra Bisht <rudra.bisht101@gmail.com>
2023-03-16 13:28:31 +0530

    Ending commit

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -63,7 +64,8 @@ passport.use(new GoogleStrategy({
     }).then(function(user) {
         if (!user) {
             user = new User({
-                googleId: profile.id
+                googleId: profile.id,
+                username: profile.displayName
             });
             user.save();
         }
@@ -97,11 +99,32 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
+    User.find({"secret": {$ne: null}}).then((foundUsers) => {
+        if(foundUsers) {
+            res.render("secrets", {usersWithSecrets: foundUsers})
+        } 
+    })
+})
+
+app.get("/submit", function(req, res){
     if(req.isAuthenticated()){
-        res.render("secrets");
+        res.render("submit");
     } else {
         res.redirect("/login");
     }
+})
+
+app.post("/submit", function(req, res){
+    const submittedSecret = req.body.secret;
+
+    User.findById(req.user.id).then((foundUser)=> {
+        if(foundUser) {
+            foundUser.secret = submittedSecret;
+            foundUser.save().then(()=>{
+                res.redirect("/secrets");
+            })
+        }
+    });
 })
 
 app.get("/logout", function(req, res){
